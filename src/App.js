8af3694f858eee785aa704d10495b03b0411aef5
Jeff King <jazfunk@gmail.com>
2020-05-23 10:34:43 -0400

    First commit, personalized.

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Evolving, living.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jeff-king.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          jeff-king.net
         </a>
       </header>
     </div>
