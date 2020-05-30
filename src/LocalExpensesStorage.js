46098f43f0e4645b24a0d1629501c9db4b555a85
Jeff King <jazfunk@gmail.com>
2020-05-30 13:31:43 -0400

    Working on LocalStroage, and expenses class component.

--- src/LocalExpensesStorage.js
+++ src/LocalExpensesStorage.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
 
-// I don't know if I'm doing this right with props
-// need to test and research
 class LocalExpensesStroage extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      expenses: props.expenses,
-    };
-  }
+  
 
-  setLocalExpenses = (newExpense) => {
+  setLocalExpenses = (props) => {
     const savedExpenses = getSavedExpenses;
-    savedExpenses.push(newExpense);
+    savedExpenses.push(props.expense);
     localStorage.setItem("expenses", JSON.stringify(savedExpenses));
   };
 
@@ -21,6 +14,7 @@ class LocalExpensesStroage extends Component {
   };
 
   render() {
+    this.setLocalExpenses
     return savedExpenses;
   }
 }
