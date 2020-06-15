9b9ba13885bd157983798346749daf85e52ebdd7
Jeff King <jazfunk@gmail.com>
2020-06-15 09:06:11 -0400

    Continue work on README.md, added AppFooter.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppHeader from './AppHeader'
 import AppBody from './AppBody'
+import AppFooter from './AppFooter'
 
 const App = () => {
   return (
     <div className="container-fluid App">
       <AppHeader />
       <AppBody />
+      <AppFooter />
     </div>
   );
 }
---
+++ src/AppFooter.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class AppFooter extends Component {
+  render() {
+    return (
+      <footer>
+        <a href="https://github.com/jazfunk">GitHub</a>&nbsp;|&nbsp;
+        <a href="https://www.linkedin.com/in/jeffking222/">LinkedIn</a>&nbsp;|&nbsp;
+        <a href="https://www.jeff-king.net/">Website</a>
+      </footer>
+    );
+  }
+}
+
+export default AppFooter
